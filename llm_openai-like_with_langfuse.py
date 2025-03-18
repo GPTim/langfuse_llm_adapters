35bc27042898cac408f80b73cff878f3248f79d3
Paolo Argilloni <p.argilloni@reply.it>
2025-03-18 11:25:40 +0100

    Set langfuse callback when receiving new message. Add hook before_cat_sends_message to add langfuse trace_id in message response.

--- llm_openai-like_with_langfuse.py
+++ llm_openai-like_with_langfuse.py
@@ -11,24 +11,28 @@ from cat.looking_glass.stray_cat import StrayCat
 
 class CustomOllamaWithLangfuse(ChatOpenAI):
 
+    langfuse_public_key = None
+    langfuse_secret_key = None
+    langfuse_host = None
+
     def __init__(self, **kwargs: Any) -> None:
         if kwargs["base_url"].endswith("/"):
             kwargs["base_url"] = kwargs["base_url"][:-1]
         
-        langfuse_handler = CallbackHandler(
-            public_key=kwargs["langfuse_public_key"],
-            secret_key=kwargs["langfuse_secret_key"],
-            host=kwargs["langfuse_host"]
-        )
-
         kwargs['model_kwargs'] = {
             "frequency_penalty": kwargs["frequency_penalty"]
         }
+        langfuse_public_key=kwargs["langfuse_public_key"]
+        langfuse_secret_key=kwargs["langfuse_secret_key"]
+        langfuse_host=kwargs["langfuse_host"]
         kwargs.pop('langfuse_public_key', None)
         kwargs.pop('langfuse_secret_key', None)
         kwargs.pop('langfuse_host', None)
         kwargs.pop('frequency_penalty', None)
-        super().__init__(api_key='ollama', callbacks=[langfuse_handler], **kwargs)
+        super().__init__(api_key='ollama', **kwargs)
+        self.langfuse_public_key=langfuse_public_key
+        self.langfuse_secret_key=langfuse_secret_key
+        self.langfuse_host=langfuse_host
 
 
 
@@ -58,13 +62,28 @@ class LLMOllamaConfigWithLangfuse(LLMSettings):
 def before_cat_reads_message(user_message_json, cat: StrayCat):
     if isinstance(cat._llm, CustomOllamaWithLangfuse):
         llm: CustomOllamaWithLangfuse = cat._llm
-        if hasattr(llm, 'callbacks'):
-            for callback in llm.callbacks:
-                if isinstance(callback, CallbackHandler):
-                    callback.user_id = cat.user_id
-                    callback.session_id = cat.user_data.id
-
+        langfuse_handler = CallbackHandler(
+            public_key=llm.langfuse_public_key,
+            secret_key=llm.langfuse_secret_key,
+            host=llm.langfuse_host,
+            user_id = cat.user_id,
+            session_id = cat.user_data.id
+        )
+        llm.callbacks = [langfuse_handler]
     return user_message_json
+
+
+@hook  # default priority = 1
+def before_cat_sends_message(message, cat: StrayCat):
+    if isinstance(cat._llm, CustomOllamaWithLangfuse):
+        this_llm = cat._llm
+        if hasattr(this_llm, "callbacks"):
+            for callback in this_llm.callbacks:
+                if isinstance(callback, CallbackHandler):
+                    trace_id = callback.trace.id
+                    message.langfuse_trace_id = trace_id
+                    
+    return message
 
 
 @hook
